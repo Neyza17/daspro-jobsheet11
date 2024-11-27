9bc81d1a5ea82b037bd3bf867b686c4630aa9d34
Neyza17 <nesyaratuanastasya@gmail.com>
2024-11-27 20:28:43 +0700

    second commit

--- Kafedua17.java
+++ Kafedua17.java
@@ -14,9 +14,54 @@ public class Kafedua17 {
         System.out.println("6. Mie Goreng - Rp 18,000");
         System.out.println("=========================");
         System.out.println("Silahkan pilih menu yang Anda inginkan!");
+        Scanner sc = new Scanner(System.in);
+System.out.print("Masukkan nomor menu pilihan Anda: ");
+int pilihan = scanner.nextInt();
+int harga = 0;
+
+switch (pilihan) {
+  case 1:
+    harga = 15000;
+    break;
+  case 2:
+    harga = 20000;
+    break;
+  case 3:
+    harga = 27000;
+    break;
+  case 4:
+    harga = 12000;
+    break;
+  case 5:
+    harga = 10000;
+    break;
+  case 6:
+    harga = 18000;
+    break;
+  default:
+    System.out.println("Pilihan tidak valid.");
+    return;
+}
+
+double totalHarga = harga;
+
+if (kodePromo.equals("DISKONS50")) {
+  System.out.println("Kode promo valid: Anda mendapatkan diskon 50%!");
+  totalHarga = harga * 0.5;
+} else if (kodePromo.equals("DISKON30")) {
+  System.out.println("Kode promo valid: Anda mendapatkan diskon 30%!");
+  totalHarga = harga * 0.7;
+} else if (kodePromo.isEmpty()) {
+  System.out.println("Kode promo tidak valid.");
+}
+
+if (isMember) {
+  System.out.println("Diskon member 10% diterapkan!");
+  totalHarga *= 0.9;
+}
+
+System.out.println("Total yang harus Anda bayar: Rp " + (int) totalHarga);
     }
-    public static void main(String[] args) {
-        Menu("Andi", true);
-    }
+    
     
 }
